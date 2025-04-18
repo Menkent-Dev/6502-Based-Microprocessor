b704b7341d6710153ef47013e711285564418333
Menkent <wtsteveofficial@outlook.com>
2025-04-18 11:11:30 +0800

    Style changes

--- src/main.cpp
+++ src/main.cpp
@@ -7,37 +7,36 @@ int main() {
 	std::cout << "[INFO/Init] Starting!\n";
 	int cycles = 12;
 	vprocessor::Memory CPU_MEM;
-	vprocessor::CPU Processor;
+	vprocessor::CPU MicroCPU;
 
-	Processor.Reset(CPU_MEM);
+	MicroCPU.Reset(CPU_MEM);
 	std::cout << "[INFO/Init] All modules are initialized\n";
 
-	// Example instruction code
 	CPU_MEM[0x0001] = 0x28;
 
-	CPU_MEM[0x8000] = Processor.NOOP;
-	CPU_MEM[0x8001] = Processor.LODA;
+	CPU_MEM[0x8000] = MicroCPU.NOOP;
+	CPU_MEM[0x8001] = MicroCPU.LODA;
 	CPU_MEM[0x8002] = 0x01;
 	CPU_MEM[0x8003] = 0x00;
-	CPU_MEM[0x8004] = Processor.INCR;
-	// End of instruction code
+	CPU_MEM[0x8004] = MicroCPU.INCR;
 
 	std::cout << "[INFO/Main] Executing program\n";
-	const int mainThread = Processor.Execute(cycles, CPU_MEM);
+	const int mainThread = MicroCPU.Execute(cycles, CPU_MEM);
 
-	uint16_t programCounter = Processor.PC;
-	uint16_t stackPointer = Processor.SP;
-	uint16_t Carry = Processor.C;
-	uint16_t Overflow = Processor.V;
-	uint16_t Negative = Processor.N;
-	uint16_t Zero = Processor.Z;
+	uint16_t programCounter = MicroCPU.PC;
+	uint16_t stackPointer = MicroCPU.SP;
+	uint16_t Carry = MicroCPU.C;
+	uint16_t Overflow = MicroCPU.V;
+	uint16_t Negative = MicroCPU.N;
+	uint16_t Zero = MicroCPU.Z;
 
-	uint16_t Index_X = Processor.X;
-	uint16_t Index_Y = Processor.Y;
-	uint16_t Accumulator = Processor.A;
+	uint16_t Index_X = MicroCPU.X;
+	uint16_t Index_Y = MicroCPU.Y;
+	uint16_t Accumulator = MicroCPU.A;
 
-	std::cout << "[INFO/Main] Program exited. Took " << cycles << " cycles to finish with " << mainThread << " cycles used\n |\n";
+	std::cout << "[INFO/Main] Program exited. Took " << cycles << " cycles to finish with " << mainThread << " cycles used\n";
 
+	std::cout << "[INF0/CPU_Emulator] PROCESSOR TRACE:\n |\n";
 	std::cout << " | PC: 0x" << decToHex(programCounter) << " SP: 0x" << decToHex(stackPointer) << "\n |\n";
 	std::cout << " | Flags:\n";
 	std::cout << " | C: " << Carry << " V: " << Overflow << " N: " << Negative << " Z: " << Zero << "\n |\n";
